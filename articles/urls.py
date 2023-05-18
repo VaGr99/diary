64aefacfeb0efbd26216622bfe0ba9fd21c2953b
VaGr99 <Grigor.Vardikian@yandex.ru>
2023-05-18 16:30:03 +0300

    add auth user

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,8 @@
-from articles.views import TaskView
+from articles.views import TaskView, CreateUserView
 from rest_framework import routers
 
 router = routers.SimpleRouter()
 router.register('tasks', TaskView)
+router.register('createUser', CreateUserView)
 
 urlpatterns = router.urls
--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, views
 
 from articles.models import Task
-from articles.serializers import TaskSerializer
+from articles.serializers import TaskSerializer, UserSerializer
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
 
 class TaskView(viewsets.ModelViewSet):
-
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+
+class CreateUserView(CreateModelMixin, GenericViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
