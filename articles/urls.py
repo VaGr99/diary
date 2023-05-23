85f8860766b17a3219dfe2139c5ffd386ce57b5c
VaGr99 <Grigor.Vardikian@yandex.ru>
2023-05-23 15:57:59 +0300

    config JWTAuth.

--- articles/urls.py
+++ articles/urls.py
@@ -1,8 +1,16 @@
 from articles.views import TaskView, CreateUserView
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.urls import path
 
 router = routers.SimpleRouter()
 router.register('tasks', TaskView)
 router.register('createUser', CreateUserView)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+              ] + router.urls
--- articles/views.py
+++ articles/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, views
 from articles.models import Task
 from articles.serializers import TaskSerializer, UserSerializer
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.mixins import CreateModelMixin
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
@@ -11,6 +12,7 @@ from .serializers import UserSerializer
 class TaskView(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class CreateUserView(CreateModelMixin, GenericViewSet):
